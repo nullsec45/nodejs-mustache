dc08210160bc14081b8ff6f1d8112fc6b6a2c0d0
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-26 14:01:30 +0700

    add test

--- test/exception.test.js
+++ test/exception.test.js
@@ -1,5 +1,11 @@
+import {callMe, MyException} from "../src/exception.js";
+
 test("exception", () => {
-    expect(() => callMe("salahx").toThrow());
+    expect(() => callMe("salah").toThrow());
     expect(() => callMe("salah").toThrow(MyException));
     expect(() => callMe("salah").toThrow("Ups kode kamu error"));
+})
+
+test("exception not happens",() =>{
+    expect(() => callMe("Benar").toBe("OK"));
 })
