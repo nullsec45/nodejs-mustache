8f55dcce6d08d4e54dfb5f52b611ad2a92f46c1f
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-26 14:04:56 +0700

    each function and duplicate test

---
+++ test/each.test.js
@@ -0,0 +1,12 @@
+import {sumAll} from "../src/sum.js";
+
+const table=[
+    [[10,10,10], 30],
+    [[10,10,10,10,10], 50],
+    [[10,10,10,10,10,10,10], 70],
+    [[10,10,10,10,10,10,10,10,10], 90]
+];
+
+test.each(table)("test sumAll(%s) should result %i", (numbers, expected) =>{
+    expect(sumAll(numbers)).toBe(expected);
+});
