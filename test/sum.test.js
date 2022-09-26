1a4b5d36ee646cbb6c1a141109099b9ecdc88dee
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-26 14:02:41 +0700

    code coverage

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,7 +1,13 @@
-import {sum} from "../src/sum.js";
+import {sum, sumAll} from "../src/sum.js";
 
 test("sum(1,2,3) must be 6",() => {
     const result=sum(1,2,3);
 
     expect(result).toBe(6);
 });
+
+
+test("test sum all", () =>{
+    const numbers=[1,2,3,4,5];
+    expect(sumAll(numbers)).toBe(15);
+})
