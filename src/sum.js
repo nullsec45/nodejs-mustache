3877db29b55b1bd4124237514b0cbfae0cd5c41f
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-28 10:40:38 +0700

    add method calculate

--- src/sum.js
+++ src/sum.js
@@ -15,3 +15,12 @@ export const sumAll=(numbers) => {
 
     return total;
 }
+
+export const calculate=(numbers, callback) =>{
+    let total=0;
+
+    for(let number of numbers){
+        total+=number;
+    }
+    callback(total);
+}
