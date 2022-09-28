76238d308850388bfd3e2ed5c84e2eb228ce08ad
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-28 10:38:50 +0700

    each object

---
+++ test/each-object.test.js
@@ -0,0 +1,24 @@
+import {sumAll} from "../src/sum.js";
+
+const table=[
+    {
+        numbers:[],
+        expected:0
+    },
+    {
+        numbers:[10],
+        expected:10       
+    },
+    {
+        numbers:[10,10],
+        expected:20
+    },
+    {
+        numbers:[10,10,10],
+        expected:30
+    }
+];
+
+test.each(table)("test sumAll($numbers) should result $expected", ({numbers, expected}) =>{
+    expect(sumAll(numbers)).toBe(expected);
+});
