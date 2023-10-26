66a9cedad238b949591e542883ced3833c2dfad0
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-27 02:44:30 +0700

    delete file test

--- test/mustache.test.js
+++
@@ -1,6 +0,0 @@
-import Mustache from "mustache";
-
-test("Menggunakan Mustache", () => {
-    const data = Mustache.render("Hello {{name}}", { name: "Fajar" });
-    expect(data).toBe("Hello Fajar");
-})
