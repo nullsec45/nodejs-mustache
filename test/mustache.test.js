6f7665413df1f6e5239f83cdcdaa09a33c26a309
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-28 16:41:53 +0700

    mustache cache

--- test/mustache.test.js
+++ test/mustache.test.js
@@ -4,3 +4,10 @@ test("Menggunakan Mustache", () => {
     const data = Mustache.render("Hello {{name}}", { name: "Fajar" });
     expect(data).toBe("Hello Fajar");
 })
+
+test("Menggunakan Mustache Cache", () => {
+    Mustache.parse("Hello {{name}}");
+
+    const data = Mustache.render("Hello {{name}}", { name: "Fajar" });
+    expect(data).toBe("Hello Fajar");
+})
